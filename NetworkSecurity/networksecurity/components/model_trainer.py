80dd5c606bde9b2ca8969203711d31a4759b1999
sudeep-tech-python-ai <shinde.s.sudeep@gmail.com>
2025-08-22 11:33:33 +0530

    changes ;

--- NetworkSecurity/networksecurity/components/model_trainer.py
+++ NetworkSecurity/networksecurity/components/model_trainer.py
@@ -22,6 +22,9 @@ from sklearn.ensemble import (
 )
 import mlflow
 
+# import dagshub 
+# dagshub.init(repo_owner='sudeep',repo_name='networksecurity',mlflow=True)
+
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTranformationArtifacts):
